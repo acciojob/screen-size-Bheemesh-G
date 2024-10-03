99aa47976b1028facbbb96e8acea3f02dc1f933b
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-03 23:03:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+window.addEventListener('resize',()=>{
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	document.getElementById('sizeInfo').textContent = `Width: ${width} and Height: ${height}`;
+});
